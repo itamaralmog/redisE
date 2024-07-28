aab1d3a5d675db9daa391a3f0bf2b6474783594c
itamaralmog <itamaralmoghermel@gmail.com>
2024-07-28 22:08:08 +0300

    More big action

--- client.cpp
+++ client.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,12 +8,16 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-#include <cassert>
 
-const size_t k_max_msg = 4096;
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
 }
 
 static int32_t read_full(int fd, char *buf, size_t n) {
@@ -41,7 +46,10 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
     return 0;
 }
 
-static int32_t query(int fd, const char *text) {
+const size_t k_max_msg = 4096;
+
+// the `query` function was simply splited into `send_req` and `read_res`.
+static int32_t send_req(int fd, const char *text) {
     uint32_t len = (uint32_t)strlen(text);
     if (len > k_max_msg) {
         return -1;
@@ -50,10 +58,10 @@ static int32_t query(int fd, const char *text) {
     char wbuf[4 + k_max_msg];
     memcpy(wbuf, &len, 4);  // assume little endian
     memcpy(&wbuf[4], text, len);
-    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
-        return err;
-    }
+    return write_all(fd, wbuf, 4 + len);
+}
 
+static int32_t read_res(int fd) {
     // 4 bytes header
     char rbuf[4 + k_max_msg + 1];
     errno = 0;
@@ -67,6 +75,7 @@ static int32_t query(int fd, const char *text) {
         return err;
     }
 
+    uint32_t len = 0;
     memcpy(&len, rbuf, 4);  // assume little endian
     if (len > k_max_msg) {
         msg("too long");
@@ -86,11 +95,10 @@ static int32_t query(int fd, const char *text) {
     return 0;
 }
 
-int main(){
+int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
-        // die("socket()");
-        perror("socket()");
+        die("socket()");
     }
 
     struct sockaddr_in addr = {};
@@ -99,38 +107,25 @@ int main(){
     addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
     int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) {
-        // die("connect");
-        perror("connect");
+        die("connect");
     }
 
-    char msg[] = "hello";
-    // write(fd, msg, strlen(msg));
+    // multiple pipelined requests
+    const char *query_list[3] = {"hello1", "hello2", "hello3"};
+    for (size_t i = 0; i < 3; ++i) {
+        int32_t err = send_req(fd, query_list[i]);
+        if (err) {
+            goto L_DONE;
+        }
+    }
+    for (size_t i = 0; i < 3; ++i) {
+        int32_t err = read_res(fd);
+        if (err) {
+            goto L_DONE;
+        }
+    }
 
-    int32_t err = query(fd, "hello1");
-    if (err) {
-        goto L_DONE;
-    }
-    err = query(fd, "hello2");
-    if (err) {
-        goto L_DONE;
-    }
-    err = query(fd, "hello3");
-    if (err) {
-        goto L_DONE;
-    }
-
-    L_DONE:
-        close(fd);
-        return 0;
-
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        // die("read");
-        perror("read");
-    }
-    printf("server says: %s\n", rbuf);
+L_DONE:
     close(fd);
-
-    
+    return 0;
 }
